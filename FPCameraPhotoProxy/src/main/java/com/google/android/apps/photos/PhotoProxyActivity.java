ab8dc1540c2239300ff4ccb124ab62993c189538
Alexander Lochmann <info@alexander-lochmann.de>
2023-10-29 18:16:06 +0100

    Make it work on Android 13

--- FPCameraPhotoProxy/src/main/java/com/google/android/apps/photos/PhotoProxyActivity.java
+++ FPCameraPhotoProxy/src/main/java/com/google/android/apps/photos/PhotoProxyActivity.java
@@ -27,28 +27,33 @@ public class PhotoProxyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityPhotoProxyBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_photo_proxy);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         Intent sourceIntent = this.getIntent();
-        Log.i(TAG, "Received intent:" + sourceIntent.toUri(0));
-        sourceIntent.setPackage("");
-        Log.i(TAG, "Modified intent:" + sourceIntent.toUri(0));
-        //this.startActivity(intent);
-        Intent targetIntent = new Intent();
-        targetIntent.setData(sourceIntent.getData());
-        targetIntent.setAction(sourceIntent.getAction());
-        targetIntent.putExtras(sourceIntent);
-        targetIntent.setFlags(sourceIntent.getFlags());
-        Log.i(TAG, "Own intent:" + targetIntent.toUri(0));
-        this.startActivity(targetIntent);
-        this.finish();
+        if (!sourceIntent.getAction().equals("android.intent.action.MAIN")) {
+            Log.i(TAG, "Received intent:" + sourceIntent.toUri(0));
+            Intent targetIntent = new Intent();
+            targetIntent.setData(sourceIntent.getData());
+            // FP stock camera uses action 'com.android.camera.action.REVIEW' which seems to be wrong.
+            // Hence, we change it to the default.
+            // Source: https://forum.fairphone.com/t/stock-camera-app-crashes-when-clicking-on-gallery-button-with-google-photos-uninstalled/83281/2
+            targetIntent.setAction(Intent.ACTION_VIEW);
+            targetIntent.putExtras(sourceIntent);
+            targetIntent.setFlags(sourceIntent.getFlags());
+            Log.i(TAG, "Own intent:" + targetIntent.toUri(0));
+            this.startActivity(targetIntent);
+            this.finish();
+        } else {
+            Log.e(TAG, "Starting this app directly is current not supported.");
+            this.finish();
+
+            binding = ActivityPhotoProxyBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+            setSupportActionBar(binding.toolbar);
+
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_photo_proxy);
+            appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        }
     }
 
     @Override
